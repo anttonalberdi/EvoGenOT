d3b5a47d15d4c30d83d7a3a3a7374485528a855c
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2019-02-14 15:32:55 +0100

    Update 16S_v3-v4.tagsteady.96.py

--- PCR mixes/16S_v3-v4.tagsteady.96.py
+++ PCR mixes/16S_v3-v4.tagsteady.96.py
@@ -1,181 +1,82 @@
-####################################
-# PCR MIX PROTOCOL FOR OPENTRONS 2 #
-####################################
-
-nsamples=96
-extrasamples=4 #extra volumes for 4 more samples, to account for pipetting error
-
-#############
-# Mastermix #
-#############
-#             1x        96x     100x
-# ddH20       13.50     1296    1350
-# 10x buffer  2.5       240     250
-# MgCl2       2.5       240     250
-# dNTP        0.5       48      50
-# BSA         1.5       144     150
-# TaqGold     0.5       48      50
-
-h20=13.5
-buffer=2.5
-mgcl=2.5
-dntp=0.5
-bsa=1.5
-taq=0.5
-
-#############################
-# To be added to each well) #
-#############################
-# Primer-F+R  2
-
-primermix=2
-
-################################
-# To be added in the Green Lab #
-################################
-# DNA         2
-
-
-####################
-
-from opentrons import labware, instruments, modules, robot
-
-#### METADATA ####
-
 metadata = {
-    'protocolName': '16S_v3-v4.tagsteady.96',
+    'protocolName': '16rRNA_V3-V4',
     'author': 'Antton Alberdi <anttonalberdi@gmail.com>',
-    'version': '1.0',
-    'date': '2019/02/02',
+    'version': '2.0',
+    'date': '2019/14/02',
     'description': 'PCR mix for 16S rRNA (v3-v4) metabarcoding of bacteria',
     'primers': 'Forward: 341F (CCTAYGGGRBGCASCAG), Reverse: R806 (GGACTACNNGGGTATCTAAT)',
+    'Comb1':'Tag1,Tag1',
+    'Comb2':'Tag2,Tag2',
+    'Comb3':'Tag3,Tag3',
+    'Comb4':'Tag4,Tag4',
+    'Comb5':'Tag5,Tag5',
+    'Comb6':'Tag6,Tag6',
+    'Comb7':'Tag7,Tag7',
+    'Comb8':'Tag8,Tag8',
+    'Comb9':'Tag9,Tag9',
+    'Comb10':'Tag10,Tag10',
+    'Comb11':'Tag11,Tag11',
+    'Comb12':'Tag12,Tag12',
+    'Comb13':'Tag13,Tag13',
+    'Comb14':'Tag14,Tag14',
+    'Comb15':'Tag15,Tag15',
+    'Comb16':'Tag16,Tag16',
+    'Comb17':'Tag17,Tag17',
+    'Comb18':'Tag18,Tag18',
+    'Comb19':'Tag19,Tag19',
+    'Comb20':'Tag20,Tag20',
+    'Comb21':'Tag21,Tag21',
+    'Comb22':'Tag22,Tag22',
+    'Comb23':'Tag23,Tag23',
+    'Comb24':'Tag24,Tag24',
+}
 }
 
+#### LIBRARIES ####
+from opentrons import labware, instruments, modules, robot
+
 #### MODULES ####
-
-#Deck 1 - Reagents and mastermixes
-#temp_deck1 = modules.load('tempdeck', '9')
-temp_plate1 = labware.load('opentrons-tuberack-2ml-eppendorf', '9', share=True) #to be changed to temp block
-    #Accessing Wells: single channel ['A1']-['D6']
-    #Spin down all reagents before placing them in the temp deck
-    #A1     H20         1500 ul 
-    #A2     10x buffer  260 ul
-    #A3     MgCl2       260 ul
-    #A4     BSA         150 ul
-    #A5     dNTP        50 ul
-    #A6     Taq         50 ul
-    #C1     Mastermix1  empty
-    #C2     Mastermix2  empty
-#temp_deck1.set_temperature(4)
-#temp_deck1.wait_for_temp()
-
-#Deck 2 - PCR plate
-#temp_deck2 = modules.load('tempdeck', '6')
-temp_plate2 = labware.load('96-PCR-flat', '6', share=True)  #to be changed to temp block
-    #Accessing Wells: single channel ['A1']-['H12'], 8-channel ['A1']-['A12']
-#temp_deck2.set_temperature(4)
-#temp_deck2.wait_for_temp()
-
-#Deck 3 - Primer combinations (combined F and R primers)
-#temp_deck3 = modules.load('tempdeck', '3')
-temp_plate3 = labware.load('opentrons-aluminum-block-PCR-strips-200ul', '3', share=True)  #to be changed to temp block
-    #Accessing Wells: single channel ['A1']-['H12'], 8-channel ['A1']-['A12']
-    #A1     Tags 1-8
-    #A2     Tags 9-16
-    #A3     Tags 17-24
-    #A4     Tags 25-32
-#temp_deck3.set_temperature(4)
-#temp_deck3.wait_for_temp()
+tube_rack1 = labware.load('opentrons-tuberack-2ml-eppendorf', '3', share=True) #Forward
+  #A1-A6: Tag1-6
+  #B1-B6: Tag7-12
+  #C1-C6: Tag13-18
+  #D1-D6: Tag19-24
+tube_rack2 = labware.load('opentrons-tuberack-2ml-eppendorf', '6', share=True) #Reverse
+  #A1-A6: Tag1-6
+  #B1-B6: Tag7-12
+  #C1-C6: Tag13-18
+  #D1-D6: Tag19-24
+tube_rack3 = labware.load('opentrons-tuberack-2ml-eppendorf', '9', share=True) #Reverse
+  #A1 = 1.5 ml ddH2O
+  #A2 = 1.5 ml ddH2O
+ice_rack1 = labware.load('PCR-strip-tall', '5', share=True)
 
 #### TIP RACKS ####
-tipracks_200 = labware.load('opentrons-tiprack-300ul', '11')
-tipracks_10 = labware.load('tiprack-10ul', '8', share=True)
+#300 ul tip racks in slots 5 and 8
+tipracks_200 = labware.load('opentrons-tiprack-300ul', '8')
 
 #### PIPETTES ####
 s50 = instruments.P50_Single(mount='left', tip_racks=[tipracks_200])
-m10 = instruments.P10_Multi(mount='right', tip_racks=[tipracks_10])
-#During calibration, place pipette tip 1-2 mm over the tubes/plates, to avoid touching the bottom of the tubes/wells
 
 ############
 # PROTOCOL #
 ############
 
-#Calculate total volumes
-h20_tot= h20 * (nsamples + extrasamples)
-buffer_tot= buffer * (nsamples + extrasamples)
-mgcl_tot= mgcl * (nsamples + extrasamples)
-bsa_tot= bsa * (nsamples + extrasamples)
-dntp_tot= dntp * (nsamples + extrasamples)
-taq_tot= taq * (nsamples + extrasamples)
-
-mmvol = (h20_tot+buffer_tot+mgcl_tot+bsa_tot+dntp_tot+taq_tot)/2 #mastermix volume per tube
-samplevol= h20+buffer+mgcl+bsa+dntp+taq #mastermix volume per well (sample)
-
-#### 1) MASTERMIX PREPARATION #### - tested with dummy reagents in 2019/02/08
+Fvol=10
+Rvol=10
+Wvol=80
+dispvol= Wvol/2
 
 #Transfer water
-  #Transfer water to the two mastermix tubes (in multiple turns), without changing the tip.
-dispvol = h20_tot/2
-s50.transfer(dispvol, temp_plate1.wells('A1'), temp_plate1.wells('C1','C2'), new_tip='never') 
+s50.transfer(dispvol, tube_rack3.wells('A1'), ice_rack1.cols('1','3','5')) 
+s50.transfer(dispvol, tube_rack3.wells('A2'), ice_rack1.cols('1','3','5')) 
 
-#Transfer 10x buffer
-  #Transfer 10x buffer to the two mastermix tubes (in multiple turns), changing the tip each time.
-dispvol = buffer_tot/2
-s50.transfer(dispvol, temp_plate1.wells('A2'), temp_plate1.wells('C1','C2'), new_tip='always')
+#Transfer F
+s50.transfer(Fvol, tube_rack1.wells('A1','A2','A3','A4','A5','A6','B1','B2'), ice_rack1.wells('A1','B1','C1','D1','E1','F1','G1','H1'), new_tip='always') 
+s50.transfer(Fvol, tube_rack1.wells('B3','B4','B5','B6','C1','C2','C3','C4'), ice_rack1.wells('A3','B3','C3','D3','E3','F3','G3','H3'), new_tip='always') 
+s50.transfer(Fvol, tube_rack1.wells('C5','C6','D1','D2','D3','D4','D5','D6'), ice_rack1.wells('A5','B5','C5','D5','E5','F5','G5','H5'), new_tip='always') 
 
-#Transfer MgCl2
-  #Transfer MgCl2 to the two mastermix tubes (in multiple turns), changing the tip each time.
-dispvol = mgcl_tot/2
-s50.transfer(dispvol, temp_plate1.wells('A3'), temp_plate1.wells('C1','C2'), new_tip='always')
-
-#Transfer dNTPs
-  #Transfer dNTPs to the two mastermix tubes (in multiple turns), changing the tip each time.
-dispvol = dntp_tot/2
-s50.transfer(dispvol, temp_plate1.wells('A4'), temp_plate1.wells('C1','C2'), new_tip='always')
-
-#Transfer BSA
-  #Transfer BSA to the two mastermix tubes (in multiple turns), changing the tip each time.
-  #The aspiration and dispensation speeds are lowered to better handle vicosity
-dispvol = bsa_tot/2
-s50.set_flow_rate(aspirate=5, dispense=15)
-s50.transfer(dispvol, temp_plate1.wells('A5'), temp_plate1.wells('C1','C2'), air_gap=10, new_tip='always')
-
-#Transfer taq
-  #Transfer Taq Polymerase to the two mastermix tubes (in multiple turns), changing the tip each time.
-  #The aspiration and dispensation speeds are lowered to better handle vicosity
-dispvol = taq_tot/2
-s50.transfer(dispvol, temp_plate1.wells('A6'), temp_plate1.wells('C1','C2'), air_gap=10, new_tip='always')
-s50.set_flow_rate(aspirate=25, dispense=50) 
-  #return to default aspiration and dispensation speeds
-
-#### 2) MASTERMIX DISTRIBUTION #### - tested with dummy reagents in 2019/02/08
-
-#Mix mastermixes
-s50.mix(4, 50, temp_plate1.wells('C1'))
-s50.mix(4, 50, temp_plate1.wells('C2'))
-
-#Transfer Mastermix1 to first 48 wells A1-H6
-s50.transfer(samplevol, temp_plate1.wells('C1'), temp_plate2.cols('1','2','3','4','5','6'))
-
-#Transfer Mastermix2 to last 48 wells A7-H12
-s50.transfer(samplevol, temp_plate1.wells('C2'), temp_plate2.cols('7','8','9','10','11','12'))
-
-#### 3) PRIMER DISTRIBUTION #### - not tested yer with dummy reagents
-
-#Tags1-8 to columns 1, 4, 7, 10
-m10.transfer(primermix, temp_plate3.wells('A1'), temp_plate2.wells('A1'))
-m10.transfer(primermix, temp_plate3.wells('A1'), temp_plate2.wells('A4'))
-m10.transfer(primermix, temp_plate3.wells('A1'), temp_plate2.wells('A7'))
-m10.transfer(primermix, temp_plate3.wells('A1'), temp_plate2.wells('A10'))
-
-#Tags9-16 to columns 2, 5, 8, 11
-m10.transfer(primermix, temp_plate3.wells('A2'), temp_plate2.wells('A2'))
-m10.transfer(primermix, temp_plate3.wells('A2'), temp_plate2.wells('A5'))
-m10.transfer(primermix, temp_plate3.wells('A2'), temp_plate2.wells('A8'))
-m10.transfer(primermix, temp_plate3.wells('A2'), temp_plate2.wells('A11'))
-
-#Tags17-24 to columns 3, 6, 9, 12
-m10.transfer(primermix, temp_plate3.wells('A3'), temp_plate2.wells('A3'))
-m10.transfer(primermix, temp_plate3.wells('A3'), temp_plate2.wells('A6'))
-m10.transfer(primermix, temp_plate3.wells('A3'), temp_plate2.wells('A9'))
-m10.transfer(primermix, temp_plate3.wells('A3'), temp_plate2.wells('A12'))
+#Transfer R
+s50.transfer(Rvol, tube_rack2.wells('A1','A2','A3','A4','A5','A6','B1','B2'), ice_rack1.wells('A1','B1','C1','D1','E1','F1','G1','H1'), new_tip='always') 
+s50.transfer(Rvol, tube_rack2.wells('B3','B4','B5','B6','C1','C2','C3','C4'), ice_rack1.wells('A3','B3','C3','D3','E3','F3','G3','H3'), new_tip='always') 
+s50.transfer(Rvol, tube_rack2.wells('C5','C6','D1','D2','D3','D4','D5','D6'), ice_rack1.wells('A5','B5','C5','D5','E5','F5','G5','H5'), new_tip='always') 
