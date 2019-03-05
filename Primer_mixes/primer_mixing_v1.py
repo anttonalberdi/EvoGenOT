96851cfa03b7d84ae1086fb7815b9f3a157cd863
“anttonalberdi” <anttonalberdi@gmail.com>
2019-03-05 13:08:44 +0100

    Primer mixing

--- Primer_mixes/primer_mixing_v1.py
+++ Primer_mixes/primer_mixing_v1.py
@@ -39,7 +39,7 @@ tube_rack3 = labware.load('opentrons-tuberack-2ml-eppendorf', '3', share=True)
   #D1-D6: Tag19-24
 
 #Primer mix rack
-temp_deck = modules.load('tempdeck', '2') #an 96-well ice rack can be also used
+temp_deck = modules.load('tempdeck', '5') #an 96-well ice rack can be also used
 temp_plate = labware.load('PCR-strip-tall', '2', share=True) #note that although no plate will be used, this is necessary
 
 #### TIP RACKS ####
@@ -81,9 +81,6 @@ water.pop(0)
 #Get number of combinations
 combnumber = len(combinations)
 
-#Subset selected combinations
-taglist = tagdata[tagdata.Combination.isin(list(tagcomb['Combination']))]
-
 #Get primer tube possition information
 tagmap = {'Tag1':'A1', 'Tag2':'A2', 'Tag3':'A3', 'Tag4':'A4', 'Tag5':'A5', 'Tag6':'A6', 'Tag7':'B1', 'Tag8':'B2', 'Tag9':'B3', 'Tag10':'B4', 'Tag11':'B5', 'Tag12':'B6', 'Tag13':'C1', 'Tag14':'C2', 'Tag15':'C3', 'Tag16':'C4', 'Tag17':'C5', 'Tag18':'C6', 'Tag19':'D1', 'Tag20':'D2', 'Tag21':'D3', 'Tag22':'D4', 'Tag23':'D5', 'Tag24':'D6'}
 forwardPos=list(forward.map(tagmap))
@@ -92,8 +89,8 @@ reversePos=list(reverse.map(tagmap))
 #Get mix well possition information
 totalmixlist = ['A1','B1','C1','D1','E1','F1','G1','H1','A3','B3','C3','D3','E3','F3','G3','H3','A5','B5','C5','D5','E5','F5','G5','H5','A7','B7','C7','D7','E7','F7','G7','H7','A9','B9','C9','D9','E9','F9','G9','H9','A11','B11','C11','D11','E11','F11','G11','H11']
 mixlist = totalmixlist[:combnumber]
-mixlist_first = totalmixlist[:combnumber/2]#first 50%
-mixlist_last = totalmixlist[(combnumber/2+1):combnumber/2]#last 50%
+mixlist_first = totalmixlist[:combnumber/2] #first 50%
+mixlist_last = totalmixlist[(combnumber/2+1):combnumber/2] #last 50%
 
 #### LIQUID HANDLING ####
 
