ba20c865125107bec9ab413f7ceec42a2a69b28c
anttonalberdi <anttonalberdi@gmail.com>
2019-03-07 11:27:17 +0100

    PCR mix changes

--- PCR_mixes/pcr_mix_96plate_v1.py
+++ PCR_mixes/pcr_mix_96plate_v1.py
@@ -73,6 +73,7 @@ with open("/root/csv/pcr_mix_96plate_v1_reagents.csv", "r") as csvfile:
         primers.append(i[6])
         samples.append(i[7])
 
+#Remove headers
 water.pop(0)
 buffer.pop(0)
 mgcl2.pop(0)
@@ -82,10 +83,20 @@ taq.pop(0)
 primers.pop(0)
 samples.pop(0)
 
+#Convert to string
+water = float(''.join(water))
+buffer = float(''.join(buffer))
+mgcl2 = float(''.join(mgcl2))
+bsa = float(''.join(bsa))
+dntp = float(''.join(dntp))
+taq = float(''.join(taq))
+primers = float(''.join(primers))
+samples = float(''.join(samples))
+
 #Load primer data file
 platecols=[]
 primercols=[]
-with open("/root/csv/pcr_mix_96plate_v1_reagents.csv", "r") as csvfile:
+with open("/root/csv/pcr_mix_96plate_v1_primers.csv", "r") as csvfile:
     settings = csv.reader(csvfile, delimiter=',')
     for i in settings:
         platecols.append(i[0])
@@ -93,8 +104,12 @@ with open("/root/csv/pcr_mix_96plate_v1_reagents.csv", "r") as csvfile:
 
 platecols.pop(0)
 platecolsStr = ','.join(platecols)
+platecolsStr = platecolsStr.replace("," , "','")
+
 primercols.pop(0)
 primercolsStr = ','.join(primercols)
+primercolsStr = primercolsStr.replace("," , "','")
+
 primerVol = primers
 
 #### PREPARATIONS ####
@@ -135,15 +150,23 @@ if bsaTot != 0:
 s50.transfer(dntpTot/2, reagent_rack.wells('A5'), reagent_rack.wells('C1'), new_tip='always')
 s50.transfer(dntpTot/2, reagent_rack.wells('A5'), reagent_rack.wells('C2'), new_tip='always')
 
-#Transfer taqTot (slower pipetting)
+#Pause to place polymerase in the rack
+robot.pause()
+
+#Transfer taqTot (slower pipetting) - mixing is very slow, need to think another strategy (mixing atomic is not working with space)
 s50.set_flow_rate(aspirate=10, dispense=10)
-s50.transfer(taqTot/2, reagent_rack.wells('A6'), reagent_rack.wells('C1'), new_tip='always')
-s50.transfer(taqTot/2, reagent_rack.wells('A6'), reagent_rack.wells('C2'), new_tip='always')
+s50.transfer(taqTot/2, reagent_rack.wells('A6'), reagent_rack.wells('C1'), new_tip='always', mix_after=(10, 50))
+s50.transfer(taqTot/2, reagent_rack.wells('A6'), reagent_rack.wells('C2'), new_tip='always', mix_after=(10, 50))
 s50.set_flow_rate(aspirate=25, dispense=50)
 
+
 #### MASTERMIX DISTRIBUTION ####
-s50.transfer(indVol, reagent_rack.wells('C1'), pcr_plate.rows('1','2','3','4','5','6'), mix_before=(10, 50))
-s50.transfer(indVol, reagent_rack.wells('C2'), pcr_plate.rows('7','8','9','10','11','12'), mix_before=(10, 50))
+s50.transfer(indVol, reagent_rack.wells('C1'), pcr_plate.cols('1','2','3','4','5','6'))
+s50.transfer(indVol, reagent_rack.wells('C2'), pcr_plate.cols('7','8','9','10','11','12'))
+
+#Pause to open primer lids
+robot.pause()
 
 #### PRIMER DISTRIBUTION ####
-m10.transfer(primerVol, primer_plate.wells(primercolsStr), pcr_plate.wells(platecolsStr), new_tip='always')
+for col in list(range(len(primercols))):
+    m10.transfer(primerVol, primer_plate.wells(primercols[col]), pcr_plate.wells(platecols[col]), new_tip='always', mix_before=(3, 10))
