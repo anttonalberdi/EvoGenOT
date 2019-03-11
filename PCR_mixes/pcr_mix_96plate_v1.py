c2c02b8dfd2e821207e6c0a1568c61b5fc6fe192
anttonalberdi <anttonalberdi@gmail.com>
2019-03-11 10:18:32 +0100

    PCR mix optimisation

--- PCR_mixes/pcr_mix_96plate_v1.py
+++ PCR_mixes/pcr_mix_96plate_v1.py
@@ -3,7 +3,7 @@ metadata = {
     'author': 'Antton Alberdi <anttonalberdi@gmail.com>',
     'version': '1.0',
     'creation_date': '2019/03/05',
-    'validation_date': '',
+    'validation_date': '2019/03/11',
     'description': 'Standard PCR mastermix preparation and distribution to plate',
 }
 
@@ -11,8 +11,8 @@ metadata = {
 
 #### CHANGELOG ####
 
+#2019-03-11, Antton - Blowout added to buffer and magnesium. Mixing to primers reduced to one. pcr_plate changed to 96-PCR-tall
 #2019-03-08, Antton - Added .top() to mastermix distribution to speed up and minimise risk of uncalibration issues
-#2019-03-08, Antton - Changed the % of extra reagents to 8%, as 4% was not enough
 
 #### LIBRARIES ####
 
@@ -38,7 +38,7 @@ reagent_rack = labware.load('opentrons-tuberack-2ml-eppendorf', '9', share=True)
   #C2 = Mastermix2
 
 #PCR plate
-pcr_plate = labware.load('PCR-strip-tall', '6', share=True)
+pcr_plate = labware.load('96-PCR-tall', '6', share=True)
 
 #Primers plate
 primer_plate = labware.load('PCR-strip-tall', '3', share=True)
@@ -122,12 +122,12 @@ primerVol = primers
 #### PREPARATIONS ####
 
 #Get total volumes (it needs ca 8% more volume for the whole plate due to the dragged liquid attached to the tips)
-waterTot = water * (samples + round(samples * 0.08))
-bufferTot = buffer * (samples + round(samples * 0.08))
-mgcl2Tot = mgcl2 * (samples + round(samples * 0.08))
-bsaTot = bsa * (samples + round(samples * 0.08))
-dntpTot = dntp * (samples + round(samples * 0.08))
-taqTot = taq * (samples + round(samples * 0.08))
+waterTot = water * (samples + round(samples * 0.04))
+bufferTot = buffer * (samples + round(samples * 0.04))
+mgcl2Tot = mgcl2 * (samples + round(samples * 0.04))
+bsaTot = bsa * (samples + round(samples * 0.04))
+dntpTot = dntp * (samples + round(samples * 0.04))
+taqTot = taq * (samples + round(samples * 0.04))
 
 #Get individual mastermix volumes
 indVol = water + buffer + mgcl2 + bsa + dntp + taq
@@ -138,17 +138,17 @@ indVol = water + buffer + mgcl2 + bsa + dntp + taq
 s50.transfer(waterTot/2, reagent_rack.wells('A1'), reagent_rack.wells('C1'))
 s50.transfer(waterTot/2, reagent_rack.wells('A1'), reagent_rack.wells('C2'))
 
-#Transfer Buffer (dispense it from the top to avoid changing tip)
-s50.transfer(bufferTot/2, reagent_rack.wells('A2'), reagent_rack.wells('C1').top())
-s50.transfer(bufferTot/2, reagent_rack.wells('A2'), reagent_rack.wells('C2').top())
+#Transfer Buffer (dispense it from the top to avoid changing tip) - NEEDS A FINAL PURGE TO AVOID DRAGGINGDROPS
+s50.transfer(bufferTot/2, reagent_rack.wells('A2'), reagent_rack.wells('C1').top(), blow_out=True)
+s50.transfer(bufferTot/2, reagent_rack.wells('A2'), reagent_rack.wells('C2').top(), blow_out=True)
 
 #Transfer MgCl2 (dispense it from the top to avoid changing tip)
-s50.transfer(mgcl2Tot/2, reagent_rack.wells('A3'), reagent_rack.wells('C1').top())
-s50.transfer(mgcl2Tot/2, reagent_rack.wells('A3'), reagent_rack.wells('C2').top())
+s50.transfer(mgcl2Tot/2, reagent_rack.wells('A3'), reagent_rack.wells('C1').top(), blow_out=True)
+s50.transfer(mgcl2Tot/2, reagent_rack.wells('A3'), reagent_rack.wells('C2').top(), blow_out=True)
 
 #Transfer BSA (slower pipetting)
 if bsaTot != 0:
-    s50.set_flow_rate(aspirate=10, dispense=10)
+    s50.set_flow_rate(aspirate=10, dispense=5)
     s50.transfer(bsaTot/2, reagent_rack.wells('A4'), reagent_rack.wells('C1'), new_tip='always')
     s50.transfer(bsaTot/2, reagent_rack.wells('A4'), reagent_rack.wells('C2'), new_tip='always')
     s50.set_flow_rate(aspirate=25, dispense=50)
@@ -158,10 +158,10 @@ s50.transfer(dntpTot/2, reagent_rack.wells('A5'), reagent_rack.wells('C1'), new_
 s50.transfer(dntpTot/2, reagent_rack.wells('A5'), reagent_rack.wells('C2'), new_tip='always')
 
 #Pause to place polymerase in the rack (until incorporating tempdecks)
-#robot.pause()
+robot.pause()
 
 #Transfer taqTot (slower pipetting) - mixing is very slow, need to think another strategy (mixing atomic is not working with space)
-s50.set_flow_rate(aspirate=10, dispense=10)
+s50.set_flow_rate(aspirate=10, dispense=5)
 s50.transfer(taqTot/2, reagent_rack.wells('A6'), reagent_rack.wells('C1'), new_tip='always')
 s50.transfer(taqTot/2, reagent_rack.wells('A6'), reagent_rack.wells('C2'), new_tip='always')
 s50.set_flow_rate(aspirate=25, dispense=50)
@@ -175,8 +175,8 @@ s50.transfer(indVol, reagent_rack.wells('C1'), pcr_plate.cols('1','2','3','4','5
 s50.transfer(indVol, reagent_rack.wells('C2'), pcr_plate.cols('7','8','9','10','11','12'))
 
 #Pause to open primer lids
-#robot.pause()
+robot.pause()
 
 #### PRIMER DISTRIBUTION ####
 for col in list(range(len(primercols))):
-    m10.transfer(primerVol, primer_plate.wells(primercols[col]), pcr_plate.wells(platecols[col]), new_tip='always', mix_before=(3, 10))
+    m10.transfer(primerVol, primer_plate.wells(primercols[col]), pcr_plate.wells(platecols[col]), new_tip='always', mix_before=(1, 10))
