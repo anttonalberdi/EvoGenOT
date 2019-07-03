d729c25771c764575543365b06bd4ffc936d3db9
anttonalberdi <anttonalberdi@gmail.com>
2019-07-03 16:25:09 +0200

    .

--- Extractions/D-Rex_initial.py
+++ Extractions/D-Rex_initial.py
@@ -41,7 +41,7 @@ mag_deck = modules.load('magdeck', '7')
 sample_plate = labware.load('1ml_PCR', '7', share=True)
 
 tipracks_200 = [labware.load('tiprack-200ul', slot, share=True)
-               for slot in ['4','5','6']]
+               for slot in ['3','4','5','6']]
 
 
 
