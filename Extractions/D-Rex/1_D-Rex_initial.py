9ef364f4678e074ebe24b5b5ec35dd69daeecced
anttonalberdi <anttonalberdi@gmail.com>
2019-08-15 09:22:05 +0200

    .

--- Extractions/D-Rex/1_D-Rex_initial.py
+++ Extractions/D-Rex/1_D-Rex_initial.py
@@ -345,7 +345,7 @@ m300.return_tip()
 ### Transfer buffer B and beads to SA12
 m300.set_flow_rate(aspirate=100, dispense=100)
 m300.pick_up_tip() # Slow down head speed 0.5X for bead handling
-m300.mix(3, Binding_buffer_vol, Binding_buffer2.nottom(1))
+m300.mix(3, Binding_buffer_vol, Binding_buffer2.bottom(1))
 max_speed_per_axis = {'x': (300), 'y': (300), 'z': (50), 'a': (50), 'b': (20), 'c': (20)}
 robot.head_speed(combined_speed=max(max_speed_per_axis.values()),**max_speed_per_axis)
 m300.set_flow_rate(aspirate=50, dispense=50)
