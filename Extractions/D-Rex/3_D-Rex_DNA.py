a572660c191701ea231b2e3c557491078d36342a
anttonalberdi <anttonalberdi@gmail.com>
2019-08-15 14:08:32 +0200

    Syntax error corrected

--- Extractions/D-Rex/3_D-Rex_DNA.py
+++ Extractions/D-Rex/3_D-Rex_DNA.py
@@ -68,7 +68,7 @@ m300 = instruments.P300_Multi(
     max_volume=200,
     aspirate_flow_rate=100,
     dispense_flow_rate=200,
-    tip_racks=([tipracks_200_1, tipracks_200_2, tipracks_200_3, tipracks_200_4])
+    tip_racks=[tipracks_200_1, tipracks_200_2, tipracks_200_3, tipracks_200_4])
 
 p1000 = instruments.P1000_Single(
     mount='left',
