5b127bcf64187dcbd01f35d5523998180476b843
anttonalberdi <anttonalberdi@gmail.com>
2019-08-27 10:54:31 +0200

    d

--- Extractions/Qiagen/DNA_purification.py
+++ Extractions/Qiagen/DNA_purification.py
@@ -214,7 +214,7 @@ m300.drop_tip()
 
 
 m300.delay(minutes=10)
-mag_deck.engage(height=32) #adjust hight
+mag_deck.engage(height=33) #adjust hight
 m300.delay(minutes=10)
 
 
@@ -920,7 +920,7 @@ m300.drop_tip()
 ### Incubating beads with elution buffer
 m300.delay(minutes=10)
 
-mag_deck.engage(height=32)
+mag_deck.engage(height=33)
 m300.delay(minutes=10)
 
 ### Transfer Elution buffer to EA1
