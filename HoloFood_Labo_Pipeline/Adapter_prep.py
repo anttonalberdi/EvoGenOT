c23dfcd81dc2ff05a103bc8a2aeeb739f268a6fb
SofiMarcos <sofia.marcos.ba@gmail.com>
2020-01-17 09:40:47 +0100

    Adapter test

---
+++ HoloFood_Labo_Pipeline/Adapter_prep.py
@@ -0,0 +1,14 @@
+#Adapter prep using Tempdeck
+from opentrons import labware, instruments, modules, robot
+from opentrons.legacy_api.modules import tempdeck
+
+temp_deck_1 = tempdeck.TempDeck()
+temp_deck_1._port = '/dev/ttyACM0'
+temp_deck1 = modules.load('tempdeck', '4')
+
+#PROTOCOL
+temp_deck_1.set_temperature(95)
+temp_deck_1.wait_for_temp()
+
+
+temp_deck_1.deactivate()
