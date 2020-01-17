4a6359be71743912871f476a6b32f96affb87f5e
anttonalberdi <anttonalberdi@gmail.com>
2020-01-17 10:47:58 +0100

    .

--- HoloFood_Labo_Pipeline/Adapter_prep.py
+++ HoloFood_Labo_Pipeline/Adapter_prep.py
@@ -4,7 +4,7 @@ from opentrons.legacy_api.modules import tempdeck
 
 temp_deck_1 = tempdeck.TempDeck()
 temp_deck_1._port = '/dev/ttyACM0'
-temp_deck1 = modules.load('tempdeck', '4')
+temp_deck1 = modules.load('tempdeck', '10')
 
 
 
